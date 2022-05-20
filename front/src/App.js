16836a7f11c6214ed15d512f6cfeff69fc023126
kangcodeis <miiren543@damu.net>
2022-05-20 17:32:37 +0900

    버튼작업했음

--- front/src/App.js
+++ front/src/App.js
@@ -6,20 +6,20 @@ import Dashboard from "./pages/admin/Dashboard";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/SignUp";
 import Main from "./pages/Main";
+import Button from "./pages/randomButton/Button";
 
 const App = () => {
     return (
         <div>
-
-                <LayoutDivider>
-                    <Routes>
-                        <Route path="/" element={<Main />} />
-                        <Route path="/admin" element={<Dashboard />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                    </Routes>
-                </LayoutDivider>
-
+            <LayoutDivider>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/admin" element={<Dashboard />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/Button" element={<Button />} />
+                </Routes>
+            </LayoutDivider>
         </div>
     );
 };
